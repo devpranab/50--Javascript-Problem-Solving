2294060259d289d0efaf1f106b49940ad67e96e1
Pranab Sarkar <86111675+devpranab@users.noreply.github.com>
2022-02-20 10:44:40 +0530

    Add files via upload
    vowelsCount

---
+++ 23_vowelsCount.js
@@ -0,0 +1,14 @@
+// find how many vowels have in a sentence
+//find these: const vowels = ["A", "E", "I", "O", "U", "a", "e", "i", "o", "u"];
+
+function countVowels(sentence){
+let count = 0;
+const letters = Array.from(sentence);
+
+letters.forEach(function(value){
+    
+})
+}
+
+const result = countVowels("I love javascript");
+console.log(result);
