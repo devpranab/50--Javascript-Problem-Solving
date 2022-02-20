b3a9f1c9848ef94b47b80b11726edf78c801dace
Pranab Sarkar <86111675+devpranab@users.noreply.github.com>
2022-02-20 09:58:24 +0530

    Add files via upload
    woodCalculator

---
+++ 20_woodCalculator.js
@@ -0,0 +1,15 @@
+/* cond:
+chair - 1 cubic meter
+table - 3 cubic meter
+bed - 5 cubic meter
+*/
+
+function woodCalculator(chair, table, bed){
+   var finalChair = chair * 1;
+   var finalTable = table * 3;
+   var finalBed = bed * 5;
+   var total = finalChair+finalTable+finalBed;
+   console.log(total);
+}
+var result = woodCalculator(2,3,2); //user 3 input
+console.log(result); //total output
