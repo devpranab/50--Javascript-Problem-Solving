e4591a2f0cd3cc15e8af98b5a3ed9344e4405435
Pranab Sarkar <86111675+devpranab@users.noreply.github.com>
2022-02-20 11:40:51 +0530

    Add files via upload
    convertBooleanAll

---
+++ 45_convertBooleanAll.js
@@ -0,0 +1,8 @@
+//convert boolen to any data type using !!
+//short form of using boolean
+
+//string to boolean
+console.log(!!"pranab");
+console.log(!! 11);
+console.log(!! 0);
+console.log(!! undefined);
